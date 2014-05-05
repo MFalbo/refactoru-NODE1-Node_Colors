166e02b9b669c8c1e63c7ef4b228f934ce27d4f7
Matthew Falbo <matthew.falbo@gmail.com>
2014-05-05 16:08:09 -0600

    Initial Commit.

---
+++ luminosity_library_test.js
@@ -0,0 +1,3 @@
+var colorUtil = require('./colorutil.js');
+
+console.log(colorUtil.luminosity(255, 239, 13));
---
+++ veryblue.js
@@ -0,0 +1,5 @@
+// PART 2
+
+var blue = require('./blue.js');
+
+console.log(blue.blue());
